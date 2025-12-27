d5500efb9cc447e65115938a244c802f0f0dbc69
Miqueas <proyectosmiqueas@gmail.com>
2025-12-27 21:39:28 +0000

    Exercise 13.8

--- index.js
+++ index.js
@@ -6,10 +6,12 @@ const { connectToDatabase } = require('./util/db')
 const { errorHandler, unknownEndpoint } = require('./util/middleware')
 
 const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
 
 app.use(express.json())
 
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
 
 app.use(unknownEndpoint)
 app.use(errorHandler)
