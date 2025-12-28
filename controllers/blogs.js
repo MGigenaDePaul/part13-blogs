47b31ec99f716ac4a0426afa0abc7eb34d9a02e1
Miqueas <proyectosmiqueas@gmail.com>
2025-12-28 02:17:10 +0000

    Exercise 13.10

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,13 +1,30 @@
+const jwt = require('jsonwebtoken')
 const router = require('express').Router()
 
 const { Blog } = require('../models')
+const { User } = require('../models')
+const { SECRET } = require('../util/config')
+
+const tokenExtractor = (req, res, next) => {
+  const authorization = req.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    try {
+      req.decodedToken = jwt.verify(authorization.substring(7), SECRET)
+    } catch{
+      return res.status(401).json({ error: 'token invalid' })
+    }
+  }  else {
+    return res.status(401).json({ error: 'token missing' })
+  }
+  next()
+}
 
 router.get('/', async (req, res) => {
   const blogs = await Blog.findAll()
   res.json(blogs)
 })
 
-router.post('/', async (req, res, next) => {
+router.post('/', tokenExtractor, async (req, res, next) => {
   try {
     const blog = await Blog.create(req.body)
     res.json(blog)
