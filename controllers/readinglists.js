9c65390315571323cbebf80c1a80b46356e0e972
Miqueas <proyectosmiqueas@gmail.com>
2025-12-28 05:45:53 +0000

    Exercise 13.22

--- controllers/readinglists.js
+++ controllers/readinglists.js
@@ -11,4 +11,24 @@ router.post('/', async (req, res, next) => {
   }
 })
 
+router.put('/:id', tokenExtractor, async (req, res, next) => {
+  try {
+    const readingList = await ReadingList.findByPk(req.params.id)
+    
+    if (!readingList) {
+      return res.status(404).json({ error: 'reading list entry not found' })
+    }
+
+    if (readingList.userId !== req.decodedToken.id) {
+      return res.status(403).json({ error: 'only the owner can update this reading list entry' })
+    }
+
+    readingList.read = req.body.read
+    await readingList.save()
+    res.json(readingList)
+  } catch(error) {
+    next(error)
+  }
+})
+
 module.exports = router
--- util/middleware.js
+++ util/middleware.js
@@ -1,3 +1,20 @@
+const jwt = require('jsonwebtoken')
+const { SECRET } = require('./config')
+
+const tokenExtractor = (req, res, next) => {
+  const authorization = req.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    try {
+      req.decodedToken = jwt.verify(authorization.substring(7), SECRET)
+    } catch{
+      return res.status(401).json({ error: 'token invalid' })
+    }
+  }  else {
+    return res.status(401).json({ error: 'token missing' })
+  }
+  next()
+}
+
 const errorHandler = (error, request, response, next) => {
   console.error(error.message)
 
@@ -24,5 +41,6 @@ const unknownEndpoint = (request, response) => {
 
 module.exports = {
   errorHandler,
-  unknownEndpoint
+  unknownEndpoint,
+  tokenExtractor
 }
